47239bfa2e2368faaf78216170a3ae8046665541
HalimaHAhmed <hanahussein6155@gmail.com>
2023-08-17 15:08:52 +0300

    working on products

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React,{useContext} from 'react';
 
-const Product = () => {
-  return <div>Product</div>;
+// import link
+
+import { Link } from 'react-router-dom';
+
+// import icons
+import {BsPlus,BSEyeFill} from 'react-icons/bs'
+
+const Product = ({product}) => {
+
+  console.log(product)
+
+  // destructure product
+
+
+  const {id,image,catergory,title,price}  = product
+  return(
+
+    <div>
+      <div>1</div>
+
+      <div>2</div>
+
+
+
+    </div>
+
+
+
+
+
+
+  )
 };
 
 export default Product;
